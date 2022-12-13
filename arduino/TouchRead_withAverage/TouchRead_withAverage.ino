2f7dd83838863b2b67dd275549423d6b6e56e17f
SabrinaVerhage <sabrinaverhage@gmail.com>
2022-12-13 09:14:53 +0100

    loop through all touchpins

--- arduino/TouchRead_withAverage/TouchRead_withAverage.ino
+++ arduino/TouchRead_withAverage/TouchRead_withAverage.ino
@@ -1,29 +1,40 @@
+#include <RunningAverage.h>
+#include "config.h"
+
 // ESP32 Touch Read Setup
 // See Touch pins at https://wiki.dfrobot.com/FireBeetle_Board_ESP32_E_SKU_DFR0654#target_5
 
+RunningAverage myRA(100);
+
+const uint8_t touchPins[] = { T0, T1, T3, T4, T5, T6 };
 
 void setup()
 {
   //for reading the values in the Serial Monitor
   Serial.begin(115200);
   delay(1000); 
+
+  myRA.clear();
   
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
-  Serial.println((String)"T0 " + touchRead(T0)); 
-  Serial.println((String)"T1 " + touchRead(T1)); 
-  //Serial.println((String)"T2 " + touchRead(T2));  //T2 doesn't seem to work
-  Serial.println((String)"T3 " + touchRead(T3)); 
-  Serial.println((String)"T4 " + touchRead(T4)); 
-  Serial.println((String)"T5 " + touchRead(T5)); 
-  Serial.println((String)"T6 " + touchRead(T6)); 
+
+  // loop through all the touchPins
+  for(int i = 0; i < 6; i++) {
+    Serial.println((String)"ID" + BOARDNUM + i + touchRead(touchPins[i]));
+    
+  }
+  
   delay(50);
 
+  myRA.addValue(touchRead(T1));
+
+  
   //this value will need to be tweaked based on cable length & plant size etc. 
-  if(touchRead(T1) < 10) {
+  if(myRA.getAverage() - touchRead(T1) > 2) {
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   } else {
     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
